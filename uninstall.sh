053b6d8f0c8cae9f4122636142fba44ab3efc9d5
Jose Cambronero <jcamsan@mit.edu>
2020-12-28 16:30:51 -0600

    uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export CHG_ROOT_DIR=$(pwd)
+source chg/scripts/defaults.sh
+
+source $(dirname ${CONDA_EXE})/../etc/profile.d/conda.sh
+conda env remove -n ${CHG_CONDA_ENV}
+rm -rf ${CHG_RESOURCES}
