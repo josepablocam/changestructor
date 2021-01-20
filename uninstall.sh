2fadfab717b60b7de650e9b37c6ac22ec336bf26
Darsh J Shah <darsh@rosetta7.csail.mit.edu>
2021-01-20 12:48:11 -0500

    check if CONDA_EXE set

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-set -eux
+set -ex
 export CHG_ROOT_DIR=$(pwd)
 source chg/scripts/defaults.sh
 
-source $(dirname ${CONDA_EXE})/../etc/profile.d/conda.sh
-conda env remove -n ${CHG_CONDA_ENV}
+if [ ! -z ${CONDA_EXE} ]
+then
+	source $(dirname ${CONDA_EXE})/../etc/profile.d/conda.sh
+	conda env remove -n ${CHG_CONDA_ENV}
+fi
 rm -rf ${CHG_RESOURCES}
